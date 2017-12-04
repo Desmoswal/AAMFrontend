54fadeb0420f2d9057a30472261f26c8b6413397
Desmoswal <desmoswal@gmail.com>
2017-12-04 12:03:22 +0100

    Calendar works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { LoginComponent } from './logging-in/login/login.component';
 import { ScheduledFlightsComponent } from './aviation/flights/scheduled-flights/scheduled-flights.component';
 import { FlightwatchComponent } from './aviation/flights/flightwatch/flightwatch.component';
 import { AviationHomeComponent } from './aviation/aviation-home/aviation-home.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { CalendarModule } from 'angular-calendar';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,12 @@ import { AviationHomeComponent } from './aviation/aviation-home/aviation-home.co
     ScheduledFlightsComponent,
     FlightwatchComponent,
     AviationHomeComponent,
+    CalendarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalendarModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
