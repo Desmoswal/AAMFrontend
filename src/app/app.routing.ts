347a3b87545a3d31f59358763caf8ab92c16bb68
dani5636 <dani5636@easv365.dk>
2017-12-04 11:45:34 +0100

    dadadad
    dada

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,11 @@
 import {AviationHomeComponent} from './aviation/aviation-home/aviation-home.component';
 import {RouterModule, Routes} from '@angular/router';
 import {NavbarComponent} from './navbar/navbar.component';
+import {FlightComponent} from './flight/flight.component';
 
 const appRoutes: Routes = [
   {path: 'home', component: AviationHomeComponent},
-  {path: 'navbarTest', component: NavbarComponent},
+  {path: 'test', component: FlightComponent},
   {path: '', component: AviationHomeComponent},
   {path: '**', redirectTo: '' }
  ];
--- src/app/flight/flight.component.ts
+++ src/app/flight/flight.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Flight} from './flight';
 
 @Component({
   selector: 'app-flight',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlightComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  flight: Flight;
+  constructor() {
+  }
 
   ngOnInit() {
   }
