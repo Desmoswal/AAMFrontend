3aa0440bbfbb51abc458c2006bb6934cb93133b7
mngdnmw <mtdunmow@gmail.com>
2017-12-08 11:44:33 +0100

    Added all the required views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 
-import { SubscriptionComponent } from './subscription/subscription/subscription.component';
+import { SubscriptionComponent } from './called-components/subscription/subscription/subscription.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './logging-in/login/login.component';
-import { HomeComponent } from './home/home.component';
-import { CalendarComponent } from './calendar/calendar.component';
+import { LoginComponent } from './main-views/login/login.component';
+import { HomeComponent } from './main-views/home/home.component';
+import { CalendarComponent } from './called-components/calendar/calendar.component';
 import { CalendarModule } from 'angular-calendar';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routing } from './app.routing';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FlightComponent } from './flight/flight.component';
-import { MapComponent } from './map/map.component';
-import { SubscriptionListComponent } from './subscription/subscription-list/subscription-list.component';
+import { NavbarComponent } from './called-components/navbar/navbar.component';
+import { FlightComponent } from './called-components/flight/flight.component';
+import { MapComponent } from './called-components/map/map.component';
+import { SubscriptionListComponent } from './called-components/subscription/subscription-list/subscription-list.component';
+import {FormsModule} from '@angular/forms';
+import { ReserveFlightComponent } from './main-views/reserve-flight/reserve-flight.component';
+import { FlightInformationComponent } from './main-views/flight-information/flight-information.component';
 
 
 
@@ -28,13 +31,16 @@ import { SubscriptionListComponent } from './subscription/subscription-list/subs
     FlightComponent,
     MapComponent,
     SubscriptionComponent,
-    SubscriptionListComponent
+    SubscriptionListComponent,
+    ReserveFlightComponent,
+    FlightInformationComponent
   ],
   imports: [
     BrowserModule,
     CalendarModule.forRoot(),
     BrowserAnimationsModule,
-    routing
+    routing,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,19 +1,16 @@
 
-import {HomeComponent} from './home/home.component';
+import {HomeComponent} from './main-views/home/home.component';
 import {RouterModule, Routes} from '@angular/router';
-import {FlightComponent} from './flight/flight.component';
-import {LoginComponent} from './logging-in/login/login.component';
-import {CalendarComponent} from './calendar/calendar.component';
-import {MapComponent} from './map/map.component';
-import {SubscriptionComponent} from './subscription/subscription/subscription.component';
+import {LoginComponent} from './main-views/login/login.component';
+import {ReserveFlightComponent} from './main-views/reserve-flight/reserve-flight.component';
+import {FlightInformationComponent} from './main-views/flight-information/flight-information.component';
 
 const appRoutes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'flight', component: FlightComponent},
-  {path: 'calendar', component: CalendarComponent},
-  {path: 'map', component: MapComponent},
-  {path: '', component: SubscriptionComponent},
+  {path: 'reserve', component: ReserveFlightComponent},
+  {path: 'info', component: FlightInformationComponent},
+  {path: '', component: HomeComponent},
   {path: '**', redirectTo: '' }
  ];
 export const routing = RouterModule.forRoot(appRoutes);
